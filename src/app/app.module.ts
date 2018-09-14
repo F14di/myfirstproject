4150c6160867d7850eec8dee0b12be6f34cafccd
fadi <ffadi14@gmail.com>
2018-09-14 15:22:31 +0300

    startedproj

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { FormsModule} from '@angular/forms';
+import { GreetingComponent } from './greeting/greeting.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    GreetingComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
